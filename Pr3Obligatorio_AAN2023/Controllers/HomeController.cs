72f5ef8fce3fc317155427a5927c9ae7b0444e29
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-07-17 01:53:40 -0300

    Cmbiar a forma facil

--- Pr3Obligatorio_AAN2023/Controllers/HomeController.cs
+++ Pr3Obligatorio_AAN2023/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pr3Obligatorio_AAN2023.Datos;
 using Pr3Obligatorio_AAN2023.Models;
 using System.Diagnostics;
 
@@ -7,15 +9,45 @@ namespace Pr3Obligatorio_AAN2023.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var peliculas = _context.Peliculas.ToList();
+            var funciones = _context.Funciones.Include(f => f.Sala).ToList();
+
+            var viewModel = new HomeViewModel
+            {
+                Peliculas = peliculas,
+                Funciones = funciones
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(int peliculaId)
+        {
+            var salas = _context.Funciones
+                .Where(f => f.Pelicula.Id == peliculaId)
+                .Select(f => f.Sala)
+                .Distinct()
+                .ToList();
+
+            var viewModel = new HomeViewModel
+            {
+                Peliculas = _context.Peliculas.ToList(),
+                Funciones = _context.Funciones.Include(f => f.Sala).ToList(),
+                Salas = salas
+            };
+
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
---
+++ Pr3Obligatorio_AAN2023/Models/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Pr3Obligatorio_AAN2023.Models;
+
+namespace Pr3Obligatorio_AAN2023.Models
+{
+    public class HomeViewModel
+    {
+        public List<Pelicula> Peliculas { get; set; }
+        public List<Funcion> Funciones { get; set; }
+        public List<Sala> Salas { get; set; }
+    }
+}
