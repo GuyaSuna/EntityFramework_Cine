76bddd379107bd9c89cc9be1afd68b77450be1c4
Alejo Spinelli <alejospinelli2@gmail.com>
2023-07-23 00:25:13 -0300

    #

--- Pr3Obligatorio_AAN2023/Controllers/ReservasController.cs
+++ Pr3Obligatorio_AAN2023/Controllers/ReservasController.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Pr3Obligatorio_AAN2023.Datos;
 using Pr3Obligatorio_AAN2023.Models;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+
+
 
 namespace Pr3Obligatorio_AAN2023.Controllers
 {
@@ -15,17 +16,23 @@ namespace Pr3Obligatorio_AAN2023.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly IMemoryCache _cache;
-        public ReservasController(ApplicationDbContext context)
+
+        public ReservasController(ApplicationDbContext context, IMemoryCache cache)
         {
             _context = context;
+            _cache = cache;
         }
 
         // GET: Reservas
         public async Task<IActionResult> Index()
         {
-              return _context.Reservas != null ? 
-                          View(await _context.Reservas.ToListAsync()) :
-                          Problem("Entity set 'ApplicationDbContext.Reservas'  is null.");
+            var reservas = await _context.Reservas
+                .Include(r => r.Usuario)
+                .Include(r => r.Funcion) // Cargar la propiedad de navegación Funcion
+                .ThenInclude(f => f.Pelicula) // Cargar la propiedad de navegación Pelicula dentro de Funcion
+                .ToListAsync();
+
+            return View(reservas);
         }
 
         // GET: Reservas/Details/5
@@ -47,26 +54,140 @@ namespace Pr3Obligatorio_AAN2023.Controllers
         }
 
         // GET: Reservas/Create
-        public IActionResult Create()
+        // Resto del código del controlador...
+
+        // GET: Reservas/Create
+        public IActionResult Create(int funcionId)
         {
-            return View();
+            var Usuario = _cache.Get("Usuario") as Usuario;
+            var Funciones = _context.Funciones.Include(r => r.Sala).Include(r => r.Pelicula).ToList();
+            var Reservas = _context.Reservas.Include(r => r.Funcion).ToList();
+
+            // Obtener la función actual
+            var funcion = Funciones.FirstOrDefault(f => f.Id == funcionId);
+
+            if (funcion == null)
+            {
+                // Si no se encontró la función con el Id proporcionado,
+                // muestra un mensaje de error o redirige a otra página
+                return RedirectToAction("Index", "Funciones");
+            }
+
+            // Obtener la cantidad total de asientos en la sala
+            int cantidadTotalAsientos = funcion.Sala.CantAsientos;
+
+            // Calcular la cantidad de asientos reservados para la función actual
+            int cantidadAsientosReservados = Reservas
+                .Where(r => r.Funcion.Id == funcionId)
+                .Sum(r => r.Asiento);
+
+            // Calcular la cantidad de asientos disponibles
+            int cantidadAsientosDisponibles = cantidadTotalAsientos - cantidadAsientosReservados;
+
+            if (cantidadAsientosDisponibles <= 0)
+            {
+                TempData["mensajeError"] = "No quedan asientos suficientes para su reserva, intente con menos asientos";
+                return RedirectToAction("Index", "Funciones");
+            }
+
+            if (Usuario != null)
+            {
+                ViewData["Usuario"] = Usuario.Id; // Asignar el ID del usuario a ViewData
+                ViewData["FuncionId"] = funcionId;
+                ViewData["FuncionTitulo"] = funcion.Pelicula.Titulo;
+                ViewData["FuncionFecha"] = funcion.Fecha;
+                ViewData["FuncionHorario"] = funcion.Horario;
+                ViewData["FuncionSalaNr"] = funcion.Sala.NroSala;
+                return View();
+            }
+            else
+            {
+                return NotFound();
+            }
         }
 
+        // Resto del código del controlador...
+
+
+
+
+
+
         // POST: Reservas/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Asiento,Precio")] Reserva reserva)
+        public async Task<IActionResult> Create(Reserva reserva)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                _context.Add(reserva);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                // Si el modelo no es válido, volvemos a mostrar el formulario con los errores
+                return View(reserva);
             }
-            return View(reserva);
+
+            var funcionId = int.Parse(Request.Form["Funcion"]);
+
+            // Obtener el valor de FuncionId desde ViewData
+            if (funcionId == null)
+            {
+                // Si no se pudo obtener el valor de funcionId correctamente, redirigir a otra página o mostrar un mensaje de error
+                Console.WriteLine("Nope x2");
+                return RedirectToAction("Index", "Reservas");
+            }
+
+            var funcion = await _context.Funciones
+               .Include(r => r.Sala)
+               .Include(r => r.Pelicula)
+               .FirstOrDefaultAsync(f => f.Id == funcionId);
+
+            if (funcion == null)
+            {
+                // Si no se encontró la función con el Id proporcionado,
+                // muestra un mensaje de error o redirige a otra página
+                return RedirectToAction("Create", "Reservas", new { funcionId });
+            }
+
+            // Obtener la cantidad total de asientos en la sala
+            int cantidadTotalAsientos = funcion.Sala.CantAsientos;
+
+            // Calcular la cantidad de asientos reservados para la función actual
+            int cantidadAsientosReservados = _context.Reservas
+                .Where(r => r.Funcion.Id == funcionId && r.Id != reserva.Id) // Excluir la reserva actual si es una edición
+                .Sum(r => r.Asiento);
+
+            // Calcular la cantidad de asientos disponibles
+            int cantidadAsientosDisponibles = cantidadTotalAsientos - cantidadAsientosReservados;
+
+            if (reserva.Asiento > cantidadAsientosDisponibles)
+            {
+                TempData["mensajeError"] = "No hay suficientes asientos disponibles para su reserva, intente con menos asientos";
+                return RedirectToAction("Create", "Reservas", new { funcionId });
+            }
+
+            // Obtener el valor de usuarioId desde el formulario
+            int usuarioId = int.Parse(Request.Form["Usuario"]);
+
+            // Buscar el Usuario en la base de datos por su Id
+            var usuario = await _context.Usuarios.FindAsync(usuarioId);
+
+            if (usuario == null)
+            {
+                // Si no se encontró el usuario con el Id proporcionado,
+                // muestra un mensaje de error o redirige a otra página
+                Console.WriteLine("nope");
+                return RedirectToAction("Index", "Reservas");
+            }
+
+            // Asignar los objetos Funcion y Usuario a la reserva
+            reserva.Funcion = funcion;
+            reserva.Usuario = usuario;
+
+            // Agregar la reserva a la base de datos
+            _context.Add(reserva);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
+
+
 
         // GET: Reservas/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -151,14 +272,14 @@ namespace Pr3Obligatorio_AAN2023.Controllers
             {
                 _context.Reservas.Remove(reserva);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool ReservaExists(int id)
         {
-          return (_context.Reservas?.Any(e => e.Id == id)).GetValueOrDefault();
+            return (_context.Reservas?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
